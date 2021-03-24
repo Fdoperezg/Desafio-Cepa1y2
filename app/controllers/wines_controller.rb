dca2477cab1cd95147209ea2e8fa59ff7b2c0677
Fernando <fp.naldo@gmail.com>
2021-03-24 13:55:56 -0300

    Added logic to edit, destroy and new wine to be only for editors

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,6 +1,5 @@
 class WinesController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_editor!
   before_action :set_wine, only: %i[ show edit update destroy ]
 
   # GET /wines or /wines.json
